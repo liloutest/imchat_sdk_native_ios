af6ea181a95ede3d1d66d56047ca9fad9242eeb6
Lilou <>
2024-12-04 13:46:43 +0800

    feat: prefix

---
+++ imchat/Classes/Common/PrefixHeader.h
@@ -0,0 +1,12 @@
+//
+//  PrefixHeader.h
+//  imchat
+//
+//  Created by Lilou on 2024/12/4.
+//
+
+#ifndef PrefixHeader_h
+#define PrefixHeader_h
+#import "ZMChatViewController.h"
+
+#endif /* PrefixHeader_h */
